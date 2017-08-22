93eff02de1d808d51f887a039ad054ad1a5d2f2f
venkatesh.n <venkatesh.n@attinadsoftware.com>
2017-08-22 15:30:42 +0530

    sfgdfg

--- PIP_LCMP/PIP_LCMP.WebApi/Startup.cs
+++ PIP_LCMP/PIP_LCMP.WebApi/Startup.cs
@@ -12,7 +12,7 @@ namespace PIP_LCMP.WebApi
     {
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
+            ConfigureAuth(app); 
         }
     }
 }
