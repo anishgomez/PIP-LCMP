0f4b7c1d74cedfa142536bf7625759f37c308a8e
Anish Gomez <anish.g@attinadsoftware.com>
2017-08-23 12:57:49 +0530

    removed unwanted usings

--- PIP_LCMP/PIP_LCMP.Repository/UnitofWork/UnitOfWork.cs
+++ PIP_LCMP/PIP_LCMP.Repository/UnitofWork/UnitOfWork.cs
@@ -1,10 +1,6 @@
 ﻿using PIP_LCMP.Repositories.ContextProvider;
 using System;
-using System.Collections.Generic;
 using System.Data.Entity;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PIP_LCMP.Repositories.UnitofWork
 {
